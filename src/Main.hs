97bfebbc286ae3a37d360ccbd26d780687f80fe8
Simon Košina <simonkosina@gmail.com>
2023-03-04 11:06:07 +0100

    Integrate parsing with I/O

--- src/Main.hs
+++ src/Main.hs
@@ -8,78 +8,35 @@ import Control.Monad (when)
 import Helper.Types (Knapsack(..), Item(..), Solution)
 import Helper.Functions (showSolution)
 
--- TODO: Extract parsing to a file
-import qualified Text.Parsec as Parsec
-import Text.Parsec((<?>))
-import qualified Data.Char as Parsec
-
--- knapsack = Knapsack {
---             maxWeight = 46,
---             minCost = 324,
---             items = [
---               Item { weight = 36, cost = 3 },
---               Item { weight = 43, cost = 1129 },
---               Item { weight = 202, cost = 94 },
---               Item { weight = 149, cost = 2084 }
---             ]
---           }
+import Parser (knapsackParser, parse)
 
 dispatch :: [(String, Knapsack -> IO ())]
 dispatch = [
-            --  ("-i", info),
+             ("-i", print),
              ("-b", showSolution . brute)
             --  ("-o", optim)
            ]
 
-parse rule = Parsec.parse rule "(source)"
-
--- knapsackParser :: Parsec.Parsec String () Knapsack
-knapsackParser :: Parsec.Parsec String () Int
-knapsackParser = do
-  Parsec.string "Knapsack {"
-  Parsec.newline
-  return 0
-
-maxWeightParser :: Parsec.Parsec String () Int
-maxWeightParser = do
-  Parsec.string "maxWeight: "
-  digits <- Parsec.many1 Parsec.digit
-  Parsec.newline
-  return (read digits :: Int)
-
-minCostParser :: Parsec.Parsec String () Int
-minCostParser = do
-  Parsec.string "minCost: "
-  digits <- Parsec.many1 Parsec.digit
-  Parsec.newline
-  return (read digits :: Int)
-
--- TODO: Expect incorrect arguments? Check #flp discord question.
 main :: IO ()
 main = do
   args <- getArgs
-  let action = head args
+  let option = head args
 
   when (null args || length args > 2) $ die "usage: flp22-fun option [input]"
-  when (action `notElem` ["-i", "-b", "-o"]) $ die $ "Unknown option '" ++ action ++ "', only -i, -b and -o are supported."
+  -- TODO: map fst dispatch instead of making a list? Same goes for error msg.
+  when (option `notElem` ["-i", "-b", "-o"]) $ die $ "Unknown option '" ++ option ++ "', only -i, -b and -o are supported."
 
-  -- if length args == 2
-  --   then do
-  --     let filePath = last args
-  --     knapsackString <- readFile filePath
-  --     return ()
-  --   else do
-  --     knapsackString <- getContents
-  --     return ()
+  let (Just action) = lookup option dispatch
 
-  knapsackString <- readFile "knapsack.txt"
-
-  print $ parse knapsackParser knapsackString
-
-  -- let (Just action) = lookup switch dispatch
-  -- putStrLn (action args)
-  -- putStrLn (action knapsack)
-
--- info _ = putStrLn "info"
-
--- optim _ = putStrLn "optim"
+  if length args == 2
+    then do
+      let filePath = last args
+      knapsackString <- readFile filePath
+      case parse knapsackParser knapsackString of
+        (Right knapsack) -> action knapsack
+        (Left e) -> print e
+    else do
+      knapsackString <- getContents
+      case parse knapsackParser knapsackString of
+        (Right knapsack) -> action knapsack
+        (Left e) -> print e
