604708953d0d8b9c83e810bae6eee8777ed32bd5
Simon Košina <simonkosina@gmail.com>
2023-03-04 10:42:41 +0100

    Finish parser

---
+++ src/Parser.hs
@@ -0,0 +1,66 @@
+module Parser (
+  knapsackParser,
+  parse
+) where
+
+import Text.Parsec ((<?>))
+import Text.Parsec qualified as Parsec
+
+import Helper.Types ( Knapsack(Knapsack), Item(Item) )
+
+parse rule text = Parsec.parse rule "(source)" text
+
+-- TODO: Switch weight/cost, maxWeight/minCost/items?
+-- FIXME: Ask about the spacing of items (is it tab, spaces, fixed number, ...?)
+-- FIXME: End of file as }EOF or }\nEOF?
+knapsackParser :: Parsec.Parsec String () Knapsack
+knapsackParser = do
+  Parsec.string "Knapsack {" >> Parsec.newline
+  mW <- maxWeightParser
+  mC <- minCostParser
+  is <- itemsParser
+  Parsec.char '}' >> Parsec.eof
+  return (Knapsack mW mC is)
+
+maxWeightParser :: Parsec.Parsec String () Int
+maxWeightParser = do
+  Parsec.string "maxWeight: "
+  digits <- Parsec.many1 Parsec.digit
+  Parsec.newline
+  return (read digits :: Int)
+
+minCostParser :: Parsec.Parsec String () Int
+minCostParser = do
+  Parsec.string "minCost: "
+  digits <- Parsec.many1 Parsec.digit
+  Parsec.newline
+  return (read digits :: Int)
+
+itemsParser :: Parsec.Parsec String () [Item]
+itemsParser = do
+  Parsec.string "items: [" >> Parsec.newline
+  items <- Parsec.many1 itemParser
+  Parsec.char ']' >> Parsec.newline
+  return items
+
+itemParser :: Parsec.Parsec String () Item
+itemParser = do
+  Parsec.tab >> Parsec.string "Item {" >> Parsec.newline
+  weight <- weightParser
+  cost <- costParser
+  Parsec.tab >> Parsec.char '}' >> Parsec.newline
+  return (Item weight cost)
+
+weightParser :: Parsec.Parsec String () Int
+weightParser = do
+  Parsec.tab >> Parsec.string "weight: "
+  digits <- Parsec.many1 Parsec.digit
+  Parsec.newline
+  return (read digits :: Int)
+
+costParser :: Parsec.Parsec String () Int
+costParser = do
+  Parsec.tab >> Parsec.string "cost: "
+  digits <- Parsec.many1 Parsec.digit
+  Parsec.newline
+  return (read digits :: Int)
